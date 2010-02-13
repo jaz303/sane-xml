5f9e1f87094ddca04a1ff076c3af5f591876f606
Jason Frame <jason@magiclamp.co.uk>
2010-02-13 03:03:56 +0000

    javadoc added

--- src/com/onehackoranother/sanexml/Parser.java
+++ src/com/onehackoranother/sanexml/Parser.java
@@ -35,7 +35,23 @@ public class Parser {
     //
     // Constructors
 
+    /**
+     * Create a new <tt>Parser</tt>
+     * @param r <tt>Reader</tt> from which XML should be read and parsed
+     *
+     * @throws IOException
+     * @throws XMLStreamException
+     */
     public Parser(Reader r) throws IOException, XMLStreamException { this(null, r); }
+
+    /**
+     * Create a new <tt>Parser</tt>
+     *
+     * @param factory <tt>XMLInputFactory</tt> used to create underlying <tt>XMLEventReader</tt>
+     * @param r <tt>Reader</tt> from which XML should be read and parsed
+     * @throws IOException
+     * @throws XMLStreamException
+     */
     public Parser(XMLInputFactory factory, Reader r) throws IOException, XMLStreamException {
         if (factory == null) {
             factory = XMLInputFactory.newInstance();
@@ -44,7 +60,23 @@ public class Parser {
         next();
     }
 
+    /**
+     * Create a new <tt>Parser</tt>
+     * @param f <tt>File</tt> from which XML should be read and parsed
+     *
+     * @throws IOException
+     * @throws XMLStreamException
+     */
     public Parser(File f) throws IOException, XMLStreamException { this(null, f); }
+
+    /**
+     * Create a new <tt>Parser</tt>
+     *
+     * @param factory <tt>XMLInputFactory</tt> used to create underlying <tt>XMLEventReader</tt>
+     * @param f <tt>File</tt> from which XML should be read and parsed
+     * @throws IOException
+     * @throws XMLStreamException
+     */
     public Parser(XMLInputFactory factory, File f) throws IOException, XMLStreamException {
         this(factory, new FileReader(f));
     }
@@ -52,10 +84,38 @@ public class Parser {
     //
     // Configuration
 
+    /**
+     * Returns the parser's whitespace skipping mode.
+     *
+     * @return parser's whitespace skipping mode
+     */
     public boolean getSkipAllWhitespace() { return skipAllWhitespace; }
+
+    /**
+     * Sets the parser's whitespace skipping mode.
+     *
+     * When <tt>true</tt>, parser will ignore any character nodes consisting only
+     * of whitespace. "Ignoreable whitespace", as defined by an XML document's DTD,
+     * is always skipped, regardless of this setting.
+     *
+     * @param s new value of whitespace skipping mode
+     */
     public void setSkipAllWhitespace(boolean s) { skipAllWhitespace = s; }
 
+    /**
+     * Returns the parser's comment skipping mode.
+     *
+     * @return parser's comment skipping mode.
+     */
     public boolean getSkipComments() { return skipComments; }
+
+    /**
+     * Sets the parser's comment skipping mode.
+     *
+     * When <tt>true</tt>, parser will ignore any comment nodes.
+     *
+     * @param s new value of comment skipping mode
+     */
     public void setSkipComments(boolean s) { skipComments = s; }
 
     //
@@ -63,12 +123,14 @@ public class Parser {
 
     /**
      * Returns the parser's current nesting depth.
+     *
      * @return the parser's current nesting depth
      */
     public int getDepth() { return depth; }
 
     /**
      * Returns the parser's current XML event.
+     * 
      * @return the parser's current XML event
      */
     public XMLEvent getEvent() { return event; }
@@ -76,6 +138,7 @@ public class Parser {
     /**
      * Returns the text of the current node, or null if the current node is
      * not a text node.
+     * 
      * @return the text of the current node
      */
     public String getText() {
@@ -86,6 +149,12 @@ public class Parser {
         }
     }
 
+    /**
+     * Returns the name of the current opening or closing tag.
+     *
+     * @return the name of the current opening/closing tag, or <tt>null</tt> if
+     *         the current node if neither an opening nor a closing tag.
+     */
     public QName currentTagName() {
         if (event.isStartElement()) {
             return event.asStartElement().getName();
@@ -96,6 +165,14 @@ public class Parser {
         }
     }
 
+    /**
+     * Returns <tt>true</tt> if the current tag name equals the give tag name.
+     *
+     * @param q tag name current node name should match
+     * @return <tt>true</tt> if current node is opening/closing tag, and tag name
+     *         matches <tt>q</tt>, or if not at opening/closing tag and <tt>q</tt>
+     *         is null. <tt>false</tt> otherwise.
+     */
     public boolean tagNameEquals(QName q) {
         QName tag = currentTagName();
         if (tag == null) {
@@ -105,6 +182,15 @@ public class Parser {
         }
     }
 
+    /**
+     * Returns <tt>true</tt> if the local part of the current tag name equals
+     * the give tag name.
+     *
+     * @param s tag name local part current node should match
+     * @return <tt>true</tt> if current node is opening/closing tag, and local
+     *         part of its name matches <tt>s</tt>, or if not at opening/closing
+     *         tag and <tt>s</tt> is null. <tt>false</tt> otherwise.
+     */
     public boolean tagNameEquals(String s) {
         QName tag = currentTagName();
         if (tag == null) {
@@ -117,6 +203,12 @@ public class Parser {
     //
     // 
 
+    /**
+     * Consume document start or throw error.
+     *
+     * @throws XMLStreamException if error in underlying XML document
+     * @throws ParseException if current node is not document start
+     */
     public void startDocument() throws XMLStreamException, ParseException {
         if (!event.isStartDocument()) {
             error("expecting document start");
@@ -124,6 +216,12 @@ public class Parser {
         next();
     }
 
+    /**
+     * Consume document end or throw error.
+     *
+     * @throws XMLStreamException if error in underlying XML document
+     * @throws ParseException if current node is not document end
+     */
     public void endDocument() throws XMLStreamException, ParseException {
         if (!event.isEndDocument()) {
             error("expecting document end");
@@ -136,18 +234,46 @@ public class Parser {
     //
     // Query current state
 
+    /**
+     * Ensure that the current node is an opening with the given name, throw
+     * an error otherwise.
+     *
+     * @param tag expected tag name
+     * @throws ParseException if current node is not matching opening tag
+     */
     public void expect(String tag) throws ParseException {
         expect(tag, "expecting opening tag: " + tag);
     }
 
+    /**
+     * Ensure that the current node is an opening with the given name, throw
+     * an error otherwise.
+     *
+     * @param tag expected tag name
+     * @param errorMessage error message passed to exception on failure
+     * @throws ParseException if current node is not matching opening tag
+     */
     public void expect(String tag, String errorMessage) throws ParseException {
         if (!atTag(tag)) throw new ParseException(errorMessage);
     }
 
+    /**
+     * Returns true if the current node is an opening tag, false otherwise.
+     *
+     * @return true if the current node is an opening tag, false otherwise
+     */
     public boolean atTag() {
         return event.isStartElement();
     }
 
+    /**
+     * Returns true if the current node is an opening tag matching any of the
+     * given tag names, false otherwise.
+     *
+     * @param arguments tag names to check for
+     * @return true if current node if opening tag with name matching any of
+     *         <tt>arguments</tt>
+     */
     public boolean atTag(String... arguments) {
         if (event.isStartElement()) {
             for (String s : arguments) {
@@ -160,10 +286,20 @@ public class Parser {
         return false;
     }
 
+    /**
+     * Returns true if current node is a closing tag, false otherwise.
+     *
+     * @return true if current node is a closing tag, false otherwise
+     */
     public boolean atClosingTag() {
         return event.isEndElement();
     }
 
+    /**
+     * Returns true if current node is text, false otherwise.
+     *
+     * @return true if current node is text, false otherwise.
+     */
     public boolean atText() {
         return event.isCharacters();
     }
@@ -171,6 +307,12 @@ public class Parser {
     //
     //
 
+    /**
+     * Consume any opening tag or throw error.
+     * 
+     * @throws XMLStreamException
+     * @throws ParseException if current node is not an opening tag
+     */
     public void accept() throws XMLStreamException, ParseException {
         if (event.getEventType() == XMLEvent.START_ELEMENT) {
             depth++;
@@ -180,6 +322,14 @@ public class Parser {
         }
     }
 
+    /**
+     * Consume opening tag with local name part matching given tag name.
+     *
+     * @param tagName expected local part of tag name
+     * @param errorMessage error message to be thrown on error
+     * @throws XMLStreamException
+     * @throws ParseException if current node is not opening tag with matching name
+     */
     public void accept(String tagName, String errorMessage) throws XMLStreamException, ParseException {
         if (atTag(tagName)) {
             accept();
@@ -188,10 +338,25 @@ public class Parser {
         }
     }
 
+    /**
+     * Consume opening tag with local name part matching given tag name.
+     *
+     * @param tagName expected local part of tag name
+     * @throws XMLStreamException
+     * @throws ParseException if current node is not opening tag with matching name
+     */
     public void accept(String tagName) throws XMLStreamException, ParseException {
         accept(tagName, "expecting tag: " + tagName);
     }
 
+    /**
+     * Consume text node or throw error.
+     *
+     * @param errorMessage error message to throw if not at text node
+     * @return contents of text node
+     * @throws XMLStreamException
+     * @throws ParseException if current node is not a text node
+     */
     public String acceptText(String errorMessage) throws XMLStreamException, ParseException {
         if (atText()) {
             String out = getText();
@@ -202,6 +367,13 @@ public class Parser {
         }
     }
 
+    /**
+     * Consume text node or throw error.
+     *
+     * @return contents of text node
+     * @throws XMLStreamException
+     * @throws ParseException if current node is not a text node
+     */
     public String acceptText() throws XMLStreamException, ParseException {
         return acceptText("expecting text");
     }
@@ -215,7 +387,8 @@ public class Parser {
      * parser.accept("foo"); parser.close();
      *
      * @throws ParseException if the current event is not a closing tag
-     * @throws XMLStreamException
+     * @throws XMLStreamException if there was an error with the underlying XML
+     *         stream, such as document not being well-formed.
      */
     public void close() throws ParseException, XMLStreamException {
         if (!this.atClosingTag()) {
@@ -230,7 +403,7 @@ public class Parser {
      * You may only call this method when the current event is an opening tag,
      * i.e. if <tt>atTag()</tt> returns <tt>true</tt>.
      *
-     * @throws ParseException
+     * @throws ParseException if current node is not an opening tag.
      * @throws XMLStreamException
      */
     public void skipTag() throws ParseException, XMLStreamException {
@@ -250,10 +423,24 @@ public class Parser {
         }
     }
 
+    /**
+     * Returns <tt>true</tt> if current node has attribute with given name.
+     * Attributes are only ever present at opening tags.
+     *
+     * @param name name of attribute for which to check
+     * @return <tt>true</tt> if node has named attribute, <tt>false</tt> otherwise.
+     */
     public boolean hasAttribute(String name) {
         return attribute(name) != null;
     }
 
+    /**
+     * Returns the value of the attribute with the given name at the current node.
+     * Attributes are only ever present at opening tags.
+     *
+     * @param name attribute name to lookup
+     * @return attribute value if present, <tt>null</tt> otherwise.
+     */
     public String attribute(String name) {
         if (!event.isStartElement()) return null;
         StartElement se = event.asStartElement();
@@ -261,7 +448,16 @@ public class Parser {
         return a == null ? null : a.getValue();
     }
 
-    public String attribute(String name, String defaultValue) throws ParseException {
+    /**
+     * Returns the value of attribute with the given name at the current node,
+     * or a given default value if the current node has no such attribute.
+     * Attributes are only ever present at opening tags.
+     *
+     * @param name attribute name to lookup
+     * @param defaultValue value to return if attribue is not present
+     * @return attribute value if present, <tt>defaultValue</tt> otherwise
+     */
+    public String attribute(String name, String defaultValue) {
         String out = attribute(name);
         return out == null ? defaultValue : out;
     }
@@ -273,7 +469,7 @@ public class Parser {
      * @return value of attribute, or empty string if attribute is not present
      * @throws ParseException
      */
-    public String attrib(String name) throws ParseException {
+    public String attrib(String name) {
         return attribute(name, "");
     }
 
